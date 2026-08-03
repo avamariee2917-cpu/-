440d8e464f909b8da64471910225e2cda1b377e6
avamariee2917-cpu <avamariee2917@gmail.com>
2026-08-02 23:14:45 -0500

    Update CustomRoleService.js

--- services/CustomRoleService.js
+++ services/CustomRoleService.js
@@ -1,67 +1,31 @@
-import { 
-    SlashCommandBuilder, 
-    PermissionFlagsBits 
-} from "discord.js";
-
+import { SlashCommandBuilder } from "discord.js";
 import { successEmbed } from "../../utils/embeds.js";
-import { InteractionHelper } from "../../utils/interactionHelper.js";
 
-// CHANGE THIS TO YOUR BOOSTER ROLE ID
 const BOOSTER_ROLE_ID = "PUT_BOOSTER_ROLE_ID_HERE";
 
-// Simple memory storage
-// Replace with database later if you want it permanent
+// Temporary storage
+// Replace with database later
 const boosterRoles = new Map();
 
 
 export default {
 
     data: new SlashCommandBuilder()
-        .setName("customrole")
-        .setDescription("Manage your booster custom role")
+        .setName("br")
+        .setDescription("Create or manage your booster role")
 
-        .addSubcommand(sub =>
-            sub
-            .setName("create")
-            .setDescription("Create your custom booster role")
-            .addStringOption(option =>
-                option
-                .setName("name")
-                .setDescription("Role name")
-                .setRequired(true)
-            )
-            .addStringOption(option =>
-                option
-                .setName("color")
-                .setDescription("Role color hex (#000000)")
-                .setRequired(false)
-            )
+        .addStringOption(option =>
+            option
+            .setName("name")
+            .setDescription("Role name")
+            .setRequired(true)
         )
 
-
-        .addSubcommand(sub =>
-            sub
-            .setName("rename")
-            .setDescription("Rename your custom role")
-            .addStringOption(option =>
-                option
-                .setName("name")
-                .setDescription("New role name")
-                .setRequired(true)
-            )
-        )
-
-
-        .addSubcommand(sub =>
-            sub
+        .addStringOption(option =>
+            option
             .setName("color")
-            .setDescription("Change your role color")
-            .addStringOption(option =>
-                option
-                .setName("color")
-                .setDescription("New hex color (#FF0000)")
-                .setRequired(true)
-            )
+            .setDescription("Role color (#527357)")
+            .setRequired(false)
         ),
 
 
@@ -69,103 +33,56 @@ export default {
 
 
 
-async execute(interaction) {
+    async execute(interaction) {
+
+        await createBoosterRole(
+            interaction,
+            interaction.options.getString("name"),
+            interaction.options.getString("color")
+        );
+
+    },
+
+
+
+    // PREFIX COMMAND
+    prefixExecute: async (interaction) => {
+
+        const args = interaction.options._hoistedOptions.map(
+            option => option.value
+        );
+
+
+        const name = args[0];
+        const color = args[1];
+
+
+        await createBoosterRole(
+            interaction,
+            name,
+            color
+        );
+
+    }
+
+};
+
+
+
+async function createBoosterRole(interaction, name, color) {
+
 
     const member = interaction.member;
 
 
-    // Check booster
-    if (!member.roles.cache.has(BOOSTER_ROLE_ID)) {
+    if(!member.roles.cache.has(BOOSTER_ROLE_ID)) {
 
         return interaction.reply({
+
             content:
-            "You must be a server booster to use custom roles.",
+            "You must be boosting the server to create a custom role.",
+
             ephemeral:true
-        });
-
-    }
-
-
-
-    const sub = interaction.options.getSubcommand();
-
-
-
-    // CREATE ROLE
-    if(sub === "create") {
-
-
-        if(boosterRoles.has(member.id)) {
-
-            return interaction.reply({
-                content:
-                "You already have a custom role. You can only own one.",
-                ephemeral:true
-            });
-
-        }
-
-
-        const name =
-        interaction.options.getString("name");
-
-
-        let color =
-        interaction.options.getString("color") || "#000000";
-
-
-
-        if(!/^#[0-9A-F]{6}$/i.test(color)) {
-
-            return interaction.reply({
-                content:
-                "Invalid color. Use a hex color like #ff0000.",
-                ephemeral:true
-            });
-
-        }
-
-
-
-        const role =
-        await interaction.guild.roles.create({
-
-            name:name,
-
-            color:color,
-
-            reason:
-            `Booster custom role for ${member.user.tag}`
-
-        });
-
-
-
-        // Put role below bot
-        const botRole =
-        interaction.guild.members.me.roles.highest.position;
-
-
-        await role.setPosition(botRole - 1);
-
-
-
-        await member.roles.add(role);
-
-
-
-        boosterRoles.set(member.id, role.id);
-
-
-
-        return interaction.reply({
-
-            embeds:[
-                successEmbed(
-                    "Custom Role Created",
-                    `Your role **${name}** has been created.`
-                )
-            ]
 
         });
 
@@ -173,18 +90,35 @@ async execute(interaction) {
 
 
 
-
-    const roleID =
-    boosterRoles.get(member.id);
-
-
-
-    if(!roleID) {
+    if(boosterRoles.has(member.id)) {
 
         return interaction.reply({
 
             content:
-            "You do not have a custom role yet. Create one first.",
+            "You already have a booster role. You can only own one.",
+
+            ephemeral:true
+
+        });
+
+    }
+
+
+
+    if(!color) {
+
+        color = "#000000";
+
+    }
+
+
+
+    if(!/^#[0-9A-F]{6}$/i.test(color)) {
+
+        return interaction.reply({
+
+            content:
+            "Invalid color. Use a hex code like `#527357`.",
 
             ephemeral:true
 
@@ -195,98 +129,47 @@ async execute(interaction) {
 
 
     const role =
-    interaction.guild.roles.cache.get(roleID);
+    await interaction.guild.roles.create({
+
+        name:name,
+
+        color:color,
+
+        reason:
+        `Booster role created by ${member.user.tag}`
+
+    });
 
 
 
-    if(!role) {
+    const botPosition =
+    interaction.guild.members.me.roles.highest.position;
 
-        boosterRoles.delete(member.id);
 
-        return interaction.reply({
-
-            content:
-            "Your custom role no longer exists.",
-
-            ephemeral:true
-
-        });
-
-    }
+    await role.setPosition(botPosition - 1);
 
 
 
-
-
-    // RENAME
-
-    if(sub === "rename") {
-
-        const name =
-        interaction.options.getString("name");
-
-
-        await role.setName(name);
+    await member.roles.add(role);
 
 
 
-        return interaction.reply({
-
-            embeds:[
-                successEmbed(
-                    "Role Updated",
-                    `Your role has been renamed to **${name}**.`
-                )
-            ]
-
-        });
-
-    }
+    boosterRoles.set(
+        member.id,
+        role.id
+    );
 
 
 
+    return interaction.reply({
 
+        embeds:[
+            successEmbed(
+                "Booster Role Created",
+                `Your role **${name}** has been created with color **${color}**.`
+            )
+        ]
 
-    // COLOR
-
-    if(sub === "color") {
-
-        const color =
-        interaction.options.getString("color");
-
-
-        if(!/^#[0-9A-F]{6}$/i.test(color)) {
-
-            return interaction.reply({
-
-                content:
-                "Invalid color. Example: #7289DA",
-
-                ephemeral:true
-
-            });
-
-        }
-
-
-
-        await role.setColor(color);
-
-
-
-        return interaction.reply({
-
-            embeds:[
-                successEmbed(
-                    "Role Color Updated",
-                    `Your role color is now **${color}**.`
-                )
-            ]
-
-        });
-
-    }
+    });
 
 }
-
-};
