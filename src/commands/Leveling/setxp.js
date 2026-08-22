f632eec2e5dd19679097752ea13399bcd8fbbf57
avamariee2917-cpu <avamariee2917@gmail.com>
2026-08-22 02:49:29 -0500

    Implement set-xp command for user XP management

--- src/commands/Leveling/setxp.js
+++ src/commands/Leveling/setxp.js
@@ -1,1 +1,65 @@
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} from 'discord.js';
 
+import {
+  setUserXP,
+  giveLevelRole,
+  calculateLevel,
+} from '../../services/levelingService.js';
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName('set-xp')
+    .setDescription('Set a user's total XP.')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('User to modify')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName('xp')
+        .setDescription('Total XP')
+        .setMinValue(0)
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.ManageGuild
+    ),
+
+  async execute(interaction) {
+    const target =
+      interaction.options.getUser('user');
+
+    const xp =
+      interaction.options.getInteger('xp');
+
+    const member =
+      await interaction.guild.members
+        .fetch(target.id)
+        .catch(() => null);
+
+    const userData =
+      setUserXP(
+        interaction.guild.id,
+        target.id,
+        xp
+      );
+
+    if (member) {
+      await giveLevelRole(
+        member,
+        calculateLevel(xp)
+      );
+    }
+
+    await interaction.reply({
+      content:
+        `${target.username}'s XP has been set to **${userData.xp}** ` +
+        `and their level is now **${userData.level}**.`,
+    });
+  },
+};
