1b3b7261a3ffcad211a90246d79aa56f94833d60
avamariee2917-cpu <avamariee2917@gmail.com>
2026-08-22 02:48:19 -0500

    Refactor reset level command for clarity and functionality

--- src/commands/Leveling/resetlevel.js
+++ src/commands/Leveling/resetlevel.js
@@ -1,90 +1,57 @@
-import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, MessageFlags } from 'discord.js';
-import { logger } from '../../utils/logger.js';
-import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
-import { checkUserPermissions } from '../../utils/permissionGuard.js';
-import { removeLevels, getUserLevelData, getLevelingConfig } from '../../services/leveling/leveling.js';
-import { createEmbed } from '../../utils/embeds.js';
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} from 'discord.js';
 
-import { InteractionHelper } from '../../utils/interactionHelper.js';
+import {
+  resetUserLevel,
+} from '../../services/levelingService.js';
+
+import {
+  LEVEL_ROLES,
+} from '../../services/levelingService.js';
+
 export default {
   data: new SlashCommandBuilder()
-    .setName('levelremove')
-    .setDescription('Remove levels from a user')
-    .addUserOption((option) =>
+    .setName('reset-level')
+    .setDescription('Reset one user to level 1.')
+    .addUserOption(option =>
       option
         .setName('user')
-        .setDescription('The user to remove levels from')
+        .setDescription('User to reset')
         .setRequired(true)
     )
-    .addIntegerOption((option) =>
-      option
-        .setName('levels')
-        .setDescription('Number of levels to remove')
-        .setRequired(true)
-        .setMinValue(1)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-    .setDMPermission(false),
-  category: 'Leveling',
+    .setDefaultMemberPermissions(
+      PermissionFlagsBits.ManageGuild
+    ),
 
-  async execute(interaction, config, client) {
-    await InteractionHelper.safeDefer(interaction);
+  async execute(interaction) {
+    const target =
+      interaction.options.getUser('user');
 
-    const hasPermission = await checkUserPermissions(
-      interaction,
-      PermissionFlagsBits.ManageGuild,
-      'You need ManageGuild permission to use this command.'
+    resetUserLevel(
+      interaction.guild.id,
+      target.id
     );
-    if (!hasPermission) return;
 
-    const levelingConfig = await getLevelingConfig(client, interaction.guildId);
-    if (!levelingConfig?.enabled) {
-      await InteractionHelper.safeEditReply(interaction, {
-        embeds: [
-          new EmbedBuilder()
-            .setColor('#f1c40f')
-            .setDescription('The leveling system is currently disabled on this server.')
-        ],
-        flags: MessageFlags.Ephemeral
-      });
-      return;
+    const member =
+      await interaction.guild.members
+        .fetch(target.id)
+        .catch(() => null);
+
+    if (member) {
+      for (const roleId of Object.values(LEVEL_ROLES)) {
+        if (member.roles.cache.has(roleId)) {
+          await member.roles
+            .remove(roleId)
+            .catch(() => {});
+        }
+      }
     }
 
-    const targetUser = interaction.options.getUser('user');
-    const levelsToRemove = interaction.options.getInteger('levels');
-
-    const member = await interaction.guild.members.fetch(targetUser.id).catch(() => null);
-    if (!member) {
-      throw new TitanBotError(
-        `User ${targetUser.id} not found in this guild`,
-        ErrorTypes.USER_INPUT,
-        'The specified user is not in this server.'
-      );
-    }
-
-    const userData = await getUserLevelData(client, interaction.guildId, targetUser.id);
-    if (userData.level === 0) {
-      throw new TitanBotError(
-        `User ${targetUser.id} is already at minimum level`,
-        ErrorTypes.VALIDATION,
-        `${targetUser.tag} is already at level 0 and cannot have levels removed.`
-      );
-    }
-
-    const updatedData = await removeLevels(client, interaction.guildId, targetUser.id, levelsToRemove);
-
-    await InteractionHelper.safeEditReply(interaction, {
-      embeds: [
-        createEmbed({
-          title: 'Levels Removed',
-          description: `Successfully removed ${levelsToRemove} levels from ${targetUser.tag}.\n**New Level:** ${updatedData.level}`,
-          color: 'success'
-        })
-      ]
+    await interaction.reply({
+      content:
+        `${target.username}'s level has been reset to **1**.`,
     });
-
-    logger.info(
-      `[ADMIN] User ${interaction.user.tag} removed ${levelsToRemove} levels from ${targetUser.tag} in guild ${interaction.guildId}`
-    );
-  }
+  },
 };
